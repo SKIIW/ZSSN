67f35c9566956d0f6de74c605185b393fc801e22
Henrique Faria <46444941+SKIIW@users.noreply.github.com>
2019-03-25 01:12:11 -0300

    Comentários finais

--- src/index.js
+++ src/index.js
@@ -2,32 +2,47 @@ var restify = require('restify');
 const mongoose = require('mongoose');
 const config = require('./config');
 
+// Mongoose Connection
 mongoose.connect('mongodb+srv://skiiw:second75@cluster0-u8rhc.mongodb.net/test?retryWrites=true')
     .then(_ => {
         const server = restify.createServer({
             name: 'ZSSN',
-            version: 'alpha'
+            version: '1.0.0'
         });
-
+        // restify on server
         server.use(restify.plugins.bodyParser())
 
+        // Users Schema 
         const userSchema = new mongoose.Schema({
             name: { type: String, required: true },
             id: { type: Number },
             age: { type: Number, required: true },
             gender: { type: String, required: true },
-            location:
+            lastlocation:
             {
                 longitude: { type: Number },
                 latitude: { type: Number },
             },
             report: { type: Number, default: 0 },
-            flag: { type: Boolean, default: false }
+            flag: { type: Boolean, default: false },
+            status: { type: String, default: "survivor" }
         })
 
         const User = mongoose.model('User', userSchema)
 
+        // Create Users
+        server.post('/users', (req, resp, next) => {
+            console.log('POST /users', req.body)
+            let user = new User(req.body)
+            user.save().then(user => {
+                resp.json(user)
+            }).catch(error => {
+                resp.status(400)
+                resp.json({ message: error.message })
+            })
+        })
 
+        // List all Users
         server.get('/users', (req, resp, next) => {
             console.log('GET /users');
             User.find().then(users => {
@@ -37,6 +52,7 @@ mongoose.connect('mongodb+srv://skiiw:second75@cluster0-u8rhc.mongodb.net/test?r
             })
         })
 
+        // Find User ID
         server.get('/users/:id', (req, resp, next) => {
 
             User.findById(req.params.id).then(user => {
@@ -51,14 +67,15 @@ mongoose.connect('mongodb+srv://skiiw:second75@cluster0-u8rhc.mongodb.net/test?r
             })
         })
 
-        server.post('/users/location', (req, resp, next) => {
-            console.log('location')
+        // Update Last Location
+        server.post('/users/lastlocation', (req, resp, next) => {
+            console.log('lastlocation')
             console.log(req.body)
             User.findById(req.body.id).then(user => {
 
                 if (req.body.location) {
-                    user.location.longitude = req.body.location.longitude;
-                    user.location.latitude = req.body.location.latitude;
+                    user.lastlocation.longitude = req.body.lastlocation.longitude;
+                    user.lastlocation.latitude = req.body.lastlocation.latitude;
                     user.save()
                     resp.json(user)
                 }
@@ -70,15 +87,21 @@ mongoose.connect('mongodb+srv://skiiw:second75@cluster0-u8rhc.mongodb.net/test?r
             })
         })
 
+        // Report Contamination
         server.post('/users/report-contamination', (req, resp, next) => {
             console.log('report-contamination')
             console.log(req.body)
             User.findById(req.body.id).then(user => {
                 user.report++
 
+                if (user.report >= 2) {
+                    user.status = "dangerous"
+                }
                 if (user.report >= 3) {
                     user.flag = true
+                    user.status = "infected"
                 }
+
                 user.save()
                 resp.json(user)
 
@@ -86,16 +109,7 @@ mongoose.connect('mongodb+srv://skiiw:second75@cluster0-u8rhc.mongodb.net/test?r
             })
         })
 
-        server.post('/users', (req, resp, next) => {
-            console.log('POST /users', req.body)
-            let user = new User(req.body)
-            user.save().then(user => {
-                resp.json(user)
-            }).catch(error => {
-                resp.status(400)
-                resp.json({ message: error.message })
-            })
-        })
+        // Delete User
         server.del('/users', (req, resp, next) => {
             User.deleteOne({ _id: req.body.id }, function (error) {
                 if (error) {
@@ -108,6 +122,7 @@ mongoose.connect('mongodb+srv://skiiw:second75@cluster0-u8rhc.mongodb.net/test?r
             })
         })
 
+        // Server start and message logging
         server.listen(3000, () => {
             console.log('Api listening on 3000');
         }, (e) => {
